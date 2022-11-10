9677efd3502d275c5fa18b94ce2c9c7134fc0513
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2022-11-10 22:29:21 +0200

    адаптивна верстка

--- js/modal.js
+++ js/modal.js
@@ -11,6 +11,7 @@
   refs.backdrop.addEventListener("click", logBackdropClick);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open");
     refs.backdrop.classList.toggle("is-hidden");
   }
 
